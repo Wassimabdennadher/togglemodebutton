b21dff8151284d539ecc3eb51936c5cff3ca3205
Wassim Abdennadher <wassim.abd@gmail.com>
2023-05-15 23:41:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react"
 
 function App() {
+
+  const initialState = { light: true }
+
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'darkness':
+        state = {...state, light: false }
+        document.body.classList.add('dark')
+        return
+      case 'lightness':
+        state = {...state, light: true }
+        document.body.classList.remove('dark')
+        return
+      default:
+        return state
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="checkbox" class="checkbox" id="checkbox" />
+        <label for="checkbox" class="checkbox-label">
+          <i class="fas fa-moon"></i>
+          <i class="fas fa-sun"></i>
+          <span class="ball" onClick={() => state.light ? dispatch({ type: 'darkness' }) : dispatch({ type: 'lightness' })}></span>
+        </label>
+      </div>
     </div>
   );
 }
-
 export default App;
